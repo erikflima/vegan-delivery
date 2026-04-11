2f99eba8c37eb5188ef411324f58ff9270cc4ddb
Erik Lima <erik.f.alves.lima@gmail.com>
2026-04-11 05:12:32 -0300

    first commit

---
+++ Microservices/Delivery-Tracking/src/main/java/com/eriklima/vegandelivery/delivery/tracking/DeliveryTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.eriklima.vegandelivery.delivery.tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryTrackingApplication.class, args);
+	}
+
+}
