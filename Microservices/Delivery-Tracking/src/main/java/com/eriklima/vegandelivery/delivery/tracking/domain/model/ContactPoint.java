0e5d1015835be00fe0682118e2130b8bf2a27fcb
Erik Lima <erik.f.alves.lima@gmail.com>
2026-04-12 10:47:28 -0300

    4.6. Implementando a camada de dominio para Delivery

---
+++ Microservices/Delivery-Tracking/src/main/java/com/eriklima/vegandelivery/delivery/tracking/domain/model/ContactPoint.java
@@ -0,0 +1,18 @@
+package com.eriklima.vegandelivery.delivery.tracking.domain.model;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode
+public class ContactPoint {
+
+    private String zipCode;
+
+    private String street;
+
+    private String number;
+
+    private String complement;
+
+    private String name;
+
+    private String phone;
+}
---
+++ Microservices/Delivery-Tracking/src/main/java/com/eriklima/vegandelivery/delivery/tracking/domain/model/DeliveryStatus.java
@@ -0,0 +1,10 @@
+package com.eriklima.vegandelivery.delivery.tracking.domain.model;
+
+public enum DeliveryStatus {
+
+    DRAFT,
+    WAITING_FOR_COURIER,
+    IN_TRANSIT,
+    DELIVERY;
+
+}
