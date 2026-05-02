26f8a8557f83f23e111bc587e14b2565876ccefc
Erik Lima <erik.f.alves.lima@gmail.com>
2026-05-02 03:25:52 -0300

    6.1. Implementando endpoint para cadastro de uma Delivery

--- Microservices/Delivery-Tracking/src/main/java/com/eriklima/vegandelivery/delivery/tracking/DeliveryTrackingApplication.java
+++ Microservices/Delivery-Tracking/src/main/java/com/eriklima/vegandelivery/delivery/tracking/DeliveryTrackingApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DeliveryTrackingApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DeliveryTrackingApplication.class, args);
 	}
 
---
+++ Microservices/Delivery-Tracking/src/main/java/com/eriklima/vegandelivery/delivery/tracking/api/controller/DeliveryController.java
@@ -0,0 +1,29 @@
+package com.eriklima.vegandelivery.delivery.tracking.api.controller;
+import com.eriklima.vegandelivery.delivery.tracking.api.model.DeliveryInput;
+import com.eriklima.vegandelivery.delivery.tracking.domain.model.Delivery;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import java.util.UUID;
+
+
+@RestController
+@RequestMapping("/api/v1/deliveries")
+public class DeliveryController {
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Delivery draft( @RequestBody @Valid DeliveryInput input ) {
+
+        return null;
+    }
+
+
+    @PutMapping("/{deliveryId}")
+    public Delivery edit( @PathVariable UUID deliveryId,
+                          @RequestBody @Valid DeliveryInput input) {
+
+        return null;
+    }
+
+}
