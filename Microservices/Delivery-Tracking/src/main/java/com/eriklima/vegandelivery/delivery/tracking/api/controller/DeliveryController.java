df3f3daaa70a3fced4ff52f17946426fedd424e4
Erik Lima <erik.f.alves.lima@gmail.com>
2026-05-03 00:56:26 -0300

    6.4. Implementando consulta de Delivery

--- Microservices/Delivery-Tracking/src/main/java/com/eriklima/vegandelivery/delivery/tracking/api/controller/DeliveryController.java
+++ Microservices/Delivery-Tracking/src/main/java/com/eriklima/vegandelivery/delivery/tracking/api/controller/DeliveryController.java
@@ -1,37 +1,55 @@
 package com.eriklima.vegandelivery.delivery.tracking.api.controller;
 import com.eriklima.vegandelivery.delivery.tracking.api.model.DeliveryInput;
 import com.eriklima.vegandelivery.delivery.tracking.domain.model.Delivery;
+import com.eriklima.vegandelivery.delivery.tracking.domain.repository.DeliveryRepository;
 import com.eriklima.vegandelivery.delivery.tracking.domain.service.DeliveryPreparationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import java.util.UUID;
-
 
 @RestController
 @RequestMapping("/api/v1/deliveries")
 @RequiredArgsConstructor
 public class DeliveryController {
 
-
     private final DeliveryPreparationService deliveryPreparationService;
+    private final DeliveryRepository deliveryRepository;
 
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Delivery draft( @RequestBody @Valid DeliveryInput input ) {
 
-        return deliveryPreparationService.draft(input);
+        return deliveryPreparationService.draft( input );
     }
 
 
     @PutMapping("/{deliveryId}")
     public Delivery edit( @PathVariable UUID deliveryId,
-                          @RequestBody @Valid DeliveryInput input ) {
+                          @RequestBody @Valid DeliveryInput input) {
 
-        return deliveryPreparationService.edit( deliveryId, input);
+        return deliveryPreparationService.edit( deliveryId, input );
     }
 
+
+    @GetMapping
+    public PagedModel<Delivery> findAll( @PageableDefault Pageable pageable ) {
+
+        return new PagedModel<>( deliveryRepository.findAll( pageable ) );
+
+    }
+
+
+    @GetMapping("/{deliveryId}")
+    public Delivery findById( @PathVariable UUID deliveryId ) {
+
+        return deliveryRepository.findById( deliveryId ).orElseThrow( ()-> new ResponseStatusException( HttpStatus.NOT_FOUND) );
+    }
 
 }
