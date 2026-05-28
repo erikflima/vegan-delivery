a9d0e581cbbb8ff9a6ac07b188ffaa579e9ebb85
Erik Lima <erik.f.alves.lima@gmail.com>
2026-05-28 04:05:54 -0300

    9.8. Integrando Service Registry com o Gateway

--- Microservices/Gateway/src/main/java/com/eriklima/gateway/GatewayApplication.java
+++ Microservices/Gateway/src/main/java/com/eriklima/gateway/GatewayApplication.java
@@ -1,7 +1,9 @@
 package com.eriklima.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayApplication {
 
